cc273067f96e14f45dd1ef8219701be36dbad5e7
Michael Morgenstern <michael.morgenstern@csmg-global.com>
2013-12-19 09:36:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kidsbook::Application.config.secret_key_base = '4466879e799b6c204b5a1dc3f3b8343298a35df47392ad4f1a772c7531f8ed1c9898686e1373321ba40073d54183216fa717c1e5682abb5e316682e79ca365eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kidsbook::Application.config.session_store :cookie_store, key: '_kidsbook_session'
